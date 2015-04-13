0a62673bb3993d8452a357d1c9623e6b3e52e0a9
mwcz <mwc@clayto.com>
2015-04-13 13:10:52 -0400

    remove a bunch of unused code from input.py

--- input.py
+++ input.py
@@ -1,25 +1,11 @@
-from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 import sys
 import freenect
-from itertools import chain
-import numpy as np
 import time
-import array
-
-stop_key = ord('q')
-low_m  = 90
-high_m = 255
-i = 0
 
 def createdepthhandler(server):
     def handledepth(dev, depth, timestamp):
-        # global i
-        # i += 1
         array1d = depth.flatten()
-        # print array1d[0]
-
         barray = bytearray(array1d)
-
         server.sendMessage(barray)
 
     return handledepth
@@ -28,17 +14,8 @@ def createdepthhandler(server):
     # barray[1] is the most significant byte
     # may need to swap them based on what js typed arrays want
 
-def handlebody(dev, ctx):
-    pass
-    # global i
-    # if i == 1:
-        # raise freenect.Kill()
-
 def startloop(server):
-    freenect.runloop(depth=createdepthhandler(server))
+    freenect.runloop(
+        depth=createdepthhandler(server)
+    )
 
-def main():
-    startloop()
-
-if __name__ == '__main__':
-    main()
