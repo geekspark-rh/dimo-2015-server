eadd01fbbd42f2ffb7ea39238f96eddf3b153058
mwcz <mwc@clayto.com>
2015-04-19 15:12:57 -0400

    add description for handledepth function

--- input.py
+++ input.py
@@ -4,6 +4,8 @@ import time
 
 def createdepthhandler(server):
     def handledepth(dev, depth, timestamp):
+        """Flatten the 2D depth array into a 1D array, convert it into a
+        bytearray, and send it to the client."""
         array1d = depth.flatten()
         barray = bytearray(array1d)
         server.sendMessage(barray)
